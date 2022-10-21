34e5c36457e4dd0795249de0e90254aa7a32202a
Wyatt <daviswy@mail.gvsu.edu>
2022-10-21 14:11:37 -0400

    Some initial steps

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname));
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static('views'));
-
 app.get('', (req, res) => {
-    res.render("hello", {})
+    res.render("hello", {});
 });
 
 app.listen(port, () => console.log(`I'm listening...`));
