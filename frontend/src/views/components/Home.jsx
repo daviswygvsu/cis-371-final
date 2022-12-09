742d49a2d1c4e1ea4842fd95636d4e7e61278ed6
Wyatt <daviswy@mail.gvsu.edu>
2022-12-09 14:44:49 -0500

    Made a bit prettier

--- frontend/src/views/components/Home.jsx
+++ frontend/src/views/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home () {
-    return(<><h1>Welcome, traveler!</h1></>);
+    return(<><h1>Welcome, traveler, to Xanathar's Vault!</h1></>);
 }
 
 export default Home;
